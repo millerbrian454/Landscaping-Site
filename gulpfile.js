794daa4c515c4d198182b63e7d43ac19fd802f3e
millerbrian454 <34964641+millerbrian454@users.noreply.github.com>
2021-09-16 21:49:52 -0400

    reset

--- gulpfile.js
+++ gulpfile.js
@@ -56,13 +56,6 @@ const jsPaths = [
             ]
         },
         {
-            dest: 'assets/js/lightbox.js',
-            src: [
-                'src/assets/js/lightbox.js',
-                'src/assets/js/components/**/*.js'
-            ]
-        },
-        {
             dest: 'assets/js/vendor.js',
             src: [
                 'src/assets/js/vendor/modernizr.js',
